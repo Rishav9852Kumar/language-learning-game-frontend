3f2a34db68d758e9b007ea5d0e0c3592e54a6797
Rishav Kumar <rishav@whydonate.com>
2023-11-05 15:34:41 +0530

    firrbase config logic seperated

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -14,13 +14,13 @@ import { initializeApp } from "firebase/app";
 //   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 // };
 const firebaseConfig = {
-  apiKey: process.env["API_KEY"],
-  authDomain: process.env["AUTH_DOMAIN"],
-  projectId: process.env["PROJECT_ID"],
-  storageBucket: process.env["STORAGE_BUCKET"],
-  messagingSenderId: process.env["MESSAGING_SENDOR_ID"],
-  appId: process.env["APP_ID"],
-  measurementId: process.env["MEASUREMENT_ID"],
+  apiKey: "AIzaSyC37R05nqLbthBkll7-vh_FYe3iR7koqOg",
+  authDomain: "language-learning-game-auth.firebaseapp.com",
+  projectId: "language-learning-game-auth",
+  storageBucket: "language-learning-game-auth.appspot.com",
+  messagingSenderId: "1061857104452",
+  appId: "1:1061857104452:web:0b2af7f961bf31e59c2211",
+  measurementId: "G-ZJBH8E0SJ7",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
