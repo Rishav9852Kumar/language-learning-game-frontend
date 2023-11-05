3acff0e257ed7313799d4abd6fd2161ea13dd80f
Rishav Kumar <rishav@whydonate.com>
2023-11-05 15:32:48 +0530

    firrbase config logic seperated

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -14,13 +14,13 @@ import { initializeApp } from "firebase/app";
 //   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 // };
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDOR_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env["API_KEY"],
+  authDomain: process.env["AUTH_DOMAIN"],
+  projectId: process.env["PROJECT_ID"],
+  storageBucket: process.env["STORAGE_BUCKET"],
+  messagingSenderId: process.env["MESSAGING_SENDOR_ID"],
+  appId: process.env["APP_ID"],
+  measurementId: process.env["MEASUREMENT_ID"],
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
